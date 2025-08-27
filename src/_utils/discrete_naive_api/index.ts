27c7ba78274d12fbd0d444c8c3a66a147a624b7d
Astroid777 <meteoroc@outlook.com>
2025-08-27 19:53:04 +0800

    feat(util): introduce message, notification, dialog, loadingBar from naive-ui

---
+++ src/_utils/discrete_naive_api/index.ts
@@ -0,0 +1,8 @@
+import { createDiscreteApi } from 'naive-ui'
+const { message, notification, dialog, loadingBar } = createDiscreteApi([
+  'message',
+  'notification',
+  'dialog',
+  'loadingBar',
+])
+export { message, notification, dialog, loadingBar }
