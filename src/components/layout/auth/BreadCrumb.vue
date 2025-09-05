78b76057cb75719847a86661896b10ad99c442a1
Astroid777 <meteoroc@outlook.com>
2025-09-05 21:12:06 +0800

    feat(navigation): add breadcrumb component to layout header

---
+++ src/components/layout/auth/BreadCrumb.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { RouterLink, useRoute } from 'vue-router'
+import { findAllParents } from '../_utils/BreadCrumbBuilder'
+import { computed } from 'vue'
+import { authRoutes } from '@/router'
+import { NBreadcrumb, NBreadcrumbItem } from 'naive-ui'
+import type { RouteRecordRaw } from 'vue-router'
+import { renderIcon } from '@/_utils/widget/renderIcon'
+import { resolveIcon, type IconNames } from '../_utils/MenuBuilder'
+const routeInfoMap: { [key: string]: RouteRecordRaw } = {}
+authRoutes.map((item) => {
+  routeInfoMap[item.name] = item
+})
+const route = useRoute()
+const breadcrumbData = computed(() => {
+  const parents = findAllParents(route.name as string, routeInfoMap)
+  parents.reverse()
+  const result = parents.map((item) => {
+    return {
+      name: item.name,
+      label: item.meta?.name,
+      icon: item.meta?.icon,
+      show: !item.meta?.hideInBreadcrumb,
+      isRoute: !item.meta?.isTransition,
+    }
+  })
+  result.push({
+    name: route.name,
+    label: route.meta?.name,
+    icon: route.meta?.icon,
+    show: !route.meta?.hideInBreadcrumb,
+    isRoute: !route.meta?.isTransition,
+  })
+  return result
+})
+</script>
+<template>
+  <n-breadcrumb separator=">">
+    <n-breadcrumb-item
+      v-for="(item, index) in breadcrumbData"
+      :key="index"
+      :clickable="item.isRoute"
+    >
+      <component :is="renderIcon(resolveIcon(item.icon as IconNames))" v-if="item.icon"></component>
+      <RouterLink v-if="item.isRoute" :to="{ name: item.name }">
+        {{ item.label }}
+      </RouterLink>
+      <template v-else>{{ item.label }}</template>
+    </n-breadcrumb-item>
+  </n-breadcrumb>
+</template>
---
+++ src/views/auth/AuthLayoutView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import clsx from 'clsx'
+import LayoutSideBar from './LayoutSideBar.vue'
+import { NScrollbar } from 'naive-ui'
+import BreadCrumb from '@/components/layout/auth/BreadCrumb.vue'
+</script>
+<template>
+  <div :class="clsx('zw-layout', 'w-full', 'h-full', 'flex', 'flex-col')">
+    <div :class="clsx('zw-header', 'w-full', 'h-14', 'p-text')">
+      <BreadCrumb></BreadCrumb>
+    </div>
+    <div :class="clsx('zw-middle', 'grow', 'w-full', 'flex')">
+      <LayoutSideBar></LayoutSideBar>
+      <n-scrollbar :class="clsx('zw-content', 'grow')" trigger="none">
+        <router-view></router-view>
+      </n-scrollbar>
+    </div>
+  </div>
+</template>
