67f25a843a55fbea8381ae0175a5c89a5ca9764a
Astroid777 <meteoroc@outlook.com>
2025-09-05 20:44:31 +0800

    feat(icons): prepare icon resource for menu

---
+++ src/components/layout/_constant/RouteIcons.ts
@@ -0,0 +1,36 @@
+import { UserData, UserMultiple, TaskComplete, TaskAssetView } from '@vicons/carbon'
+import { Fluid20Regular, ApprovalsApp16Regular } from '@vicons/fluent'
+import { NodeIndexOutlined, UserSwitchOutlined } from '@vicons/antd'
+import {
+  RealEstateAgentOutlined,
+  AssignmentIndOutlined,
+  ManageAccountsOutlined,
+  ApprovalFilled,
+  FeaturedPlayListOutlined,
+  InsertInvitationOutlined,
+  MenuBookTwotone,
+  MapsHomeWorkOutlined,
+  BusinessCenterOutlined,
+  DesignServicesTwotone,
+} from '@vicons/material'
+
+export const routeIcons = {
+  UserData,
+  RealEstateAgentOutlined,
+  AssignmentIndOutlined,
+  ManageAccountsOutlined,
+  UserMultiple,
+  ApprovalFilled,
+  TaskComplete,
+  TaskAssetView,
+  Fluid20Regular,
+  NodeIndexOutlined,
+  FeaturedPlayListOutlined,
+  InsertInvitationOutlined,
+  MenuBookTwotone,
+  MapsHomeWorkOutlined,
+  ApprovalsApp16Regular,
+  BusinessCenterOutlined,
+  UserSwitchOutlined,
+  DesignServicesTwotone,
+} as const
