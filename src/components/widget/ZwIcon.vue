20374947d0c5bed5c13580922fbe70b088bac3f0
Astroid777 <meteoroc@outlook.com>
2025-09-08 17:10:04 +0800

    feat(icons): integrate iconfont with reusable ZwIcon component

---
+++ src/components/widget/ZwIcon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { NIcon } from 'naive-ui'
+const props = defineProps<{
+  name: string
+  size?: number
+}>()
+</script>
+<template>
+  <n-icon :size="props.size !== undefined ? props.size : 22">
+    <svg class="icon" aria-hidden="true">
+      <use :xlink:href="`#icon-${props.name}`"></use>
+    </svg>
+  </n-icon>
+</template>
