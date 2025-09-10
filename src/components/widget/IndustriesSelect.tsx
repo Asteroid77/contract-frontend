1c8d481563b20ab057f3a936209e340ad604cc33
Astroid777 <meteoroc@outlook.com>
2025-09-10 10:49:26 +0800

    chore(components): enhance IndustriesSelect with native NSelect props support

--- src/components/widget/IndustriesSelect.tsx
+++ src/components/widget/IndustriesSelect.tsx
@@ -1,7 +1,8 @@
-import { NSelect } from 'naive-ui'
+import { NSelect, selectProps } from 'naive-ui'
 import { defineComponent } from 'vue'
 import { Industries } from './constant/IndustriesContant'
 export default defineComponent({
+  props: selectProps,
   setup(_, { attrs, slots }) {
     return () => <NSelect options={Industries} {...attrs} v-slots={slots}></NSelect>
   },
