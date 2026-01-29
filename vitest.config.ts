e9c2049861026b28b2fc0acb4dd3d8c8fb644eaf
meteor <meteoroc@outlook.com>
2026-01-29 10:24:08 +0800

    chore(tests): update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,17 @@
 import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import { defineConfig, configDefaults } from 'vitest/config'
+import { mergeConfig } from 'vite'
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
+export default defineConfig(() => {
+  const resolvedViteConfig =
+    typeof viteConfig === 'function' ? viteConfig({ mode: 'test', command: 'serve' }) : viteConfig
+
+  return mergeConfig(resolvedViteConfig, {
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
-  }),
-)
+  })
+})
