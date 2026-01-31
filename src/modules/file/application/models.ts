f40ccd699af55afb7b9cb6a94e6215790cb32fa5
meteor <meteoroc@outlook.com>
2026-01-31 21:44:09 +0800

    refactor(file): replace OssCallbackDTO with OssCallbackView

--- src/modules/file/application/models.ts
+++ src/modules/file/application/models.ts
@@ -4,8 +4,6 @@ export type OssCallbackView = Omit<DomainOssCallbackDTO, 'path'> & {
   accessUrl: string
 }
 
-export type OssCallbackDTO = OssCallbackView
-
 export const toOssCallbackView = (dto: DomainOssCallbackDTO): OssCallbackView => {
   const { path, ...rest } = dto
   return {
