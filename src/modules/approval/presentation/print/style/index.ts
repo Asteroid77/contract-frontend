4a5ac04f523b9ebdf32cf9c16e8ab00b03b03192
meteor <meteoroc@outlook.com>
2026-02-04 01:29:10 +0800

    refactor(print): switch to diff-check print styles

--- src/modules/approval/presentation/print/style/index.ts
+++ src/modules/approval/presentation/print/style/index.ts
@@ -1,12 +1,7 @@
-import printCore from './PrintCore.css?raw'
-
-import attachmentStyles from '@/modules/approval/presentation/approval/styles/AttachmentApprovalDiff.css?raw'
-import cardStyles from '@/modules/approval/presentation/approval/styles/FileItemCard.css?raw'
+import diffCheckPrint from '@/modules/shared/presentation/diff-check/styles/print.css?raw'
 
 export const getPrintStyles = () => {
   return `
-    ${printCore}
-    ${attachmentStyles}
-    ${cardStyles}
+    ${diffCheckPrint}
   `
 }
